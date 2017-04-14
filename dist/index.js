e613c6917ab4d0dcffbb6ffd850d4678bf647e85
Charles Louw <charles.louw@gmail.com>
2017-04-14 08:10:26 +0200

    fixed untracked files

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var _ = require("lodash");
-function IsBlank(obj, isEmpty) {
-    if (isEmpty === void 0) { isEmpty = false; }
-    return _.isNil(obj) || (isEmpty && _.isEmpty(obj));
-}
-exports.IsBlank = IsBlank;
-//# sourceMappingURL=index.js.map
